1528165f348288e1f9539a56e33264bee6e59ab3
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-28 04:39:08 +0300

    Add unit tests for Add and Max methods in Math class

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnsTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result == 3);
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}
