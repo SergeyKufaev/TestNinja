552633830c2e1c2cb70310146b2eba1d832d5797
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-30 00:36:03 +0300

    Add unit tests for FizzBuzz class

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutout_InputIsDivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result == "FizzBuzz");
+        }
+
+        [Test]
+        public void GetOutout_InputIsDivisibleBy3Only_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result == "Fizz");
+        }
+
+        [Test]
+        public void GetOutout_InputIsDivisibleBy5Only_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result == "Buzz");
+        }
+
+        [Test]
+        public void GetOutout_InputIsNotDivisibleBy3Or5_ReturnsTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(1);
+
+            Assert.That(result == "1");
+        }
+    }
+}
