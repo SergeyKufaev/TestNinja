0a34e9253b8d398de114ec8239c20007f733376f
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-29 16:34:16 +0300

    Add unit test for HtmlFormatter

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,27 @@
+﻿
+using NUnit.Framework;
+
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_EnclosesTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific
+            Assert.That(result == "<strong>abc</strong>");
+
+            // More general
+            //Assert.That(result, Does.StartWith("<strong>"));
+            //Assert.That(result, Does.Contain("abc"));
+            //Assert.That(result, Does.EndWith("</strong>"));
+        }
+    }
+}
