203055c03340d03b1d4a7ce6780a01bd4009cf63
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-30 04:25:38 +0300

    Add unit tests for Product class

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Applies30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Applies30PercentDiscount2()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
