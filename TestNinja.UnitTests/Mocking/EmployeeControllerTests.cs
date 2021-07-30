6111b80519816ad038ae9fbf32fe219ad3b81d79
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-30 06:50:05 +0300

    Add unit tests for EmployeeController class

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class EmployeeControllerTests
+    {
+        Mock<IEmployeeStorage> _storage;
+        EmployeeController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IEmployeeStorage>();
+            _controller = new EmployeeController(_storage.Object);
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeletesTheEmployeeFromDb()
+        {
+            _controller.DeleteEmployee(1);
+
+            _storage.Verify(s => s.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnsRedirectResult()
+        {
+            var result = _controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
