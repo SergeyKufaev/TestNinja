e09e0c455114e5c2f2442c892f21d05f2f0249df
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-30 06:18:05 +0300

    Add unit tests for InstallerHelper class

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+using Moq;
+
+using NUnit.Framework;
+
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class InstallerHelperTests
+    {
+        Mock<IFileDownloader> _fileDownloader;
+        InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnsFalse()
+        {
+            _fileDownloader.Setup(fd =>
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result == false);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnsTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result == true);
+        }
+    }
+}
---
+++ TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace TestNinja.Mocking
+{
+    public interface IFileDownloader
+    {
+        void DownloadFile(string uri, string path);
+    }
+
+    class FileDownloader : IFileDownloader
+    {
+        public void DownloadFile(string uri, string path)
+        {
+            var client = new WebClient();
+            client.DownloadFile(uri, path);
+        }
+    }
+}
